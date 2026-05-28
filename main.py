15efc339087ce93aa7ad392e21d3071ad5f45e06
zeroextwenty-eng <zeroextwenty@gmail.com>
2026-05-28 19:47:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -160,5 +160,5 @@ async def monitor():
         logging.info("Reconnecting in 5 seconds...")
         await asyncio.sleep(5)
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
     asyncio.run(monitor())
